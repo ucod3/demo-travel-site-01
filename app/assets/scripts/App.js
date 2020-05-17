25e02ff25cc3c9ece26206a5f7e7cbdd4ab288e8
Usman Butt <usman.butt@dcmail.ca>
2020-05-17 12:00:08 -0400

    Completed lazy loading of the images.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,5 @@
 import '../css/styles.css';
+import 'lazysizes';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
