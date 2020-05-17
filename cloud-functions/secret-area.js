929657f3d02810763d11ed7bf40b406721991b0d
Usman Butt <usman.butt@dcmail.ca>
2020-05-17 14:46:24 -0400

    Update cloud function to check password.

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,25 @@
 exports.handler = function (event, context, callback) {
-	callback(null, {
-		statusCode: 200,
-		body: 'Welcome to the super secret area.',
-	});
+	const secretContent = `
+    <h3>Welcome to The Secret Area</h3>
+    <p>Sky is blue like <strong>blue</strong> bubble gum and two plus three is five.</p>
+    `;
+
+	let body;
+
+	if (event.body) {
+		body = JSON.parse(event.body);
+	} else {
+		body = {};
+	}
+
+	if (body.password == 'javascript') {
+		callback(null, {
+			statusCode: 200,
+			body: secretContent,
+		});
+	} else {
+		callback(null, {
+			statusCode: 401,
+		});
+	}
 };
